3fe8b1a000cf838bf47f679925bc2467f74bf5dc
AlexanderPease <alexander@usv.com>
2014-09-09 18:17:18 -0400

    added google contacts scope

--- app/googleauth.py
+++ app/googleauth.py
@@ -6,7 +6,8 @@ from oauth2client.client import OAuth2WebServerFlow
 
 OAUTH_SCOPE = ('https://www.googleapis.com/auth/gmail.modify '
               'https://www.googleapis.com/auth/userinfo.email '
-              'https://www.googleapis.com/auth/userinfo.profile') 
+              'https://www.googleapis.com/auth/userinfo.profile '
+              'https://www.googleapis.com/auth/contacts.readonly') 
               #'https://www.googleapis.com/auth/plus.profile.emails.read '
               #'https://www.googleapis.com/auth/plus.me')
 
