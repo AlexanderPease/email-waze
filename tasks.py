1639fbfa9963b3451e8905bb5141cde30c4e1c2a
AlexanderPease <alexander@usv.com>
2014-09-19 15:22:15 -0400

    working periodic dummy task

--- tasks.py
+++ tasks.py
@@ -1,11 +1,17 @@
 from celery import Celery
-import celery
-import iron_celery
+from celery.task import periodic_task
+from datetime import timedelta
+import logging
 import settings
 
 celery = Celery('tasks', 
   broker=settings.get('rabbitmq_bigwig_url'))
 
-@celery.task
+
 def add(x, y):
     return x + y
+
+#@celery.task
+@periodic_task(run_every=timedelta(seconds=10))
+def print_add():
+  print add(53, 67)
