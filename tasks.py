419c7795c3821e296b5ca4681469ab6f2252d51e
AlexanderPease <alexander@usv.com>
2014-09-19 18:34:28 -0400

    debugging revoking celery tasks in the cloud

--- tasks.py
+++ tasks.py
@@ -13,8 +13,11 @@ def add(x, y):
     return x + y
 
 #@celery.task.periodic_task(run_every=timedelta(seconds=10))
-@celery.task
 def run_google_contacts_job():
+    celery_google_contacts_job.delay()
+
+@celery.task
+def celery_google_contacts_job():
     logging.info("Celery worker starting tasks.run_google_contacts_job()")
     google_contacts_job()
     logging.info("Celery worker finished tasks.run_google_contacts_job()")
