262e25815b2f0aad1ee7076d4109f8b41e551454
AlexanderPease <alexander@usv.com>
2014-10-09 18:40:48 -0400

    Abstract population of Connection fields into an instance method of that class

--- db/connectiondb.py
+++ db/connectiondb.py
@@ -3,6 +3,7 @@ from mongoengine import *
 from userdb import User
 from profiledb import Profile
 import logging, datetime
+from app import gmail
 
 mongo_database = settings.get('mongo_database')
 connect('connection', host=mongo_database['host'])
@@ -34,3 +35,57 @@ class Connection(Document):
 
     def __str__(self):
         return 'Connection: User %s <-> Profile %s' % (self.user, self.profile)
+
+
+    def populate_from_gmail(gmail_service):
+        """
+        This function searches entire inbox to populate fields,
+        therefore it is not optimized for speed.
+
+        Requires gmail_service to be from self.user, or else will populate
+        field incorrectly. 
+
+        Args:
+            gmail_service: a Google API service object authed in with self.user
+        """
+        # See if any messages match query
+        emails_in = gmail.ListMessagesMatchingQuery(service=gmail_service, 
+                                                    user_id='me', 
+                                                    query= "from:" + p.email)
+        emails_out = gmail.ListMessagesMatchingQuery(service=gmail_service, 
+                                                    user_id='me', 
+                                                    query= "to:" + p.email)
+
+        # Emails in fields
+        if emails_in and len(emails_in) > 0:
+            c.total_emails_in = len(emails_in)
+
+            # Get dates of latest emails in and out
+            try:
+                latest_email_in = gmail.GetMessage(service=gmail_service, 
+                                    user_id='me', 
+                                    msg_id=emails_in[0]['id'])
+                latest_email_in_header = gmail.GetMessageHeader(latest_email_in)
+                c.latest_email_in_date = gmail.ParseDate(latest_email_in_header['Date'])
+            except:
+                logging.warning('latest_email_in not added')
+        else:
+            c.total_emails_in = 0
+
+        # Emails out fields
+        if emails_out and len(emails_out) > 0:
+            c.total_emails_out = len(emails_out)
+            try:
+                latest_email_out = gmail.GetMessage(service=gmail_service, 
+                                    user_id='me', 
+                                    msg_id=emails_out[0]['id'])
+                latest_email_out_header = gmail.GetMessageHeader(latest_email_out)
+                c.latest_email_out_date = gmail.ParseDate(latest_email_out_header['Date'])
+            except:
+                logging.warning('latest_email_out not added')
+        else:
+            c.total_emails_out = 0
+
+        # Finished with all Connection fields
+        c.last_updated = datetime.datetime.now()
+        c.save()
--- tasks.py
+++ tasks.py
@@ -11,7 +11,6 @@ from db.profiledb import Profile
 from db.userdb import User
 from db.connectiondb import Connection
 
-from app import gmail
 import gdata.contacts.client
 
 #from app.gmail import GmailJob
@@ -25,114 +24,21 @@ app = Celery('tasks',
 def add(x, y):
     return x + y
 
-@app.task
-def test_worker(u):
-    logging.info('Onboarding new user %s' % u)
-    # Get Contacts and Gmail access for user
-    gd_client= u.get_gd_client()
-    gmail_service = u.get_service(service_type='gmail')
-
-    if gd_client and gmail_service:
-        query = gdata.contacts.client.ContactsQuery()
-        # GetContacts defaults to return 25 contacts, so extend query first
-        query.max_results = 99999 
-
-        try: 
-            feed = gd_client.GetContacts(q=query)
-        except:
-            logging.warning('User %s does not have Google Contacts API permission' % user)
-            return
-
-        # Add all email addresses from feed on best effort basis
-        for i, entry in enumerate(feed.entry):
-            # Get name, if it's in the entry
-            try:
-                name = entry.name.full_name.text
-            except:
-                name = None
-
-            # Contact must have a name to be added to database
-            if name:
-                for email in entry.email:
-                    if email.primary and email.primary == 'true' and email.address:
-                        # Entry has passed tests and will now be added to database
-                        email = email.address
-                        logging.info('Adding: %s <%s>' % (name, email))
-
-                        # The following lines could be condensed if 
-                        # Profile.get_or_create() was working. 
-                        try:
-                            p = Profile.objects.get(email=email)
-                            logging.info(p)
-                        except DoesNotExist:
-                            p = Profile.add_new(name=name, email=email)
-                            logging.info(p)
-                        logging.info('above line should show profile')
-
-                        # Ensure p was succesfully created before 
-                        # adding a Connection
-                        if p:
-                            # Search Connection database to see if this is a new contact
-                            c, created_flag = Connection.objects.get_or_create(user=u,
-                                                                            profile=p)
-                            # If newly created Connection, fill it in by
-                            # searching Gmail API
-                            if not created_flag:
-                                logging.info('%s already exists, not updating' % c)
-                            else:
-                                logging.info('Created %s' % c)
-                                # See if any messages match query
-                                emails_in = gmail.ListMessagesMatchingQuery(service=gmail_service, 
-                                                                            user_id='me', 
-                                                                            query= "from:" + p.email)
-                                emails_out = gmail.ListMessagesMatchingQuery(service=gmail_service, 
-                                                                            user_id='me', 
-                                                                            query= "to:" + p.email)
-
-                                # Emails in fields
-                                if emails_in and len(emails_in) > 0:
-                                    c.total_emails_in = len(emails_in)
-
-                                    # Get dates of latest emails in and out
-                                    try:
-                                        latest_email_in = gmail.GetMessage(service=gmail_service, 
-                                                            user_id='me', 
-                                                            msg_id=emails_in[0]['id'])
-                                        latest_email_in_header = gmail.GetMessageHeader(latest_email_in)
-                                        c.latest_email_in_date = gmail.ParseDate(latest_email_in_header['Date'])
-                                    except:
-                                        logging.warning('latest_email_in not added')
-                                else:
-                                    c.total_emails_in = 0
-
-                                # Emails out fields
-                                if emails_out and len(emails_out) > 0:
-                                    c.total_emails_out = len(emails_out)
-                                    try:
-                                        latest_email_out = gmail.GetMessage(service=gmail_service, 
-                                                            user_id='me', 
-                                                            msg_id=emails_out[0]['id'])
-                                        latest_email_out_header = gmail.GetMessageHeader(latest_email_out)
-                                        c.latest_email_out_date = gmail.ParseDate(latest_email_out_header['Date'])
-                                    except:
-                                        logging.warning('latest_email_out not added')
-                                else:
-                                    c.total_emails_out = 0
-
-                                # Finished with all Connection fields
-                                logging.info(c.latest_email_out_date)
-                                logging.info(c.latest_email_in_date)
-                                c.last_updated = datetime.datetime.now()
-                                c.save()
-                        else:
-                            logging.warning('Profile and Connection not added')
-    else:
-        logging.warning('User %s could not log in to Gmail or Contacts API', user)
-
 
 @app.task
 def onboard_user(u):
+    """
+    Onboards a new user by
+    1. Searches through Google Contacts and creates new Profile objects if that
+        email address does not yet exist in global database. 
+    2. Creating Correspondence objecst for all Profiles this User is 
+        connected to
+
+    Args:
+        u is a new User to the app. 
+    """
     logging.info('Onboarding new user %s' % u)
+
     # Get Contacts and Gmail access for user
     gd_client= u.get_gd_client()
     gmail_service = u.get_service(service_type='gmail')
@@ -175,61 +81,21 @@ def onboard_user(u):
                         logging.info('above line should show profile')
 
                         # Ensure p was succesfully created before 
-                        # adding a Connection
+                        # adding a Connection. Profile.add_new() sometimes fails
                         if p:
                             # Search Connection database to see if this is a new contact
                             c, created_flag = Connection.objects.get_or_create(user=u,
                                                                             profile=p)
-
                             # If newly created Connection, fill it in by
                             # searching Gmail API
                             if not created_flag:
                                 logging.info('%s already exists, not updating' % c)
                             else:
                                 logging.info('Created %s' % c)
-                                # See if any messages match query
-                                emails_in = gmail.ListMessagesMatchingQuery(service=gmail_service, 
-                                                                            user_id='me', 
-                                                                            query= "from:" + p.email)
-                                emails_out = gmail.ListMessagesMatchingQuery(service=gmail_service, 
-                                                                            user_id='me', 
-                                                                            query= "to:" + p.email)
 
-                                # Emails in fields
-                                if emails_in and len(emails_in) > 0:
-                                    c.total_emails_in = len(emails_in)
-
-                                    # Get dates of latest emails in and out
-                                    try:
-                                        latest_email_in = gmail.GetMessage(service=gmail_service, 
-                                                            user_id='me', 
-                                                            msg_id=emails_in[0]['id'])
-                                        latest_email_in_header = gmail.GetMessageHeader(latest_email_in)
-                                        c.latest_email_in_date = gmail.ParseDate(latest_email_in_header['Date'])
-                                    except:
-                                        logging.warning('latest_email_in not added')
-                                else:
-                                    c.total_emails_in = 0
-
-                                # Emails out fields
-                                if emails_out and len(emails_out) > 0:
-                                    c.total_emails_out = len(emails_out)
-                                    try:
-                                        latest_email_out = gmail.GetMessage(service=gmail_service, 
-                                                            user_id='me', 
-                                                            msg_id=emails_out[0]['id'])
-                                        latest_email_out_header = gmail.GetMessageHeader(latest_email_out)
-                                        c.latest_email_out_date = gmail.ParseDate(latest_email_out_header['Date'])
-                                    except:
-                                        logging.warning('latest_email_out not added')
-                                else:
-                                    c.total_emails_out = 0
-
-                                # Finished with all Connection fields
-                                logging.info(c.latest_email_out_date)
-                                logging.info(c.latest_email_in_date)
-                                c.last_updated = datetime.datetime.now()
-                                c.save()
+                                # Updates fields of c by searching through users'
+                                # entire Gmail inbox 
+                                c.populate_from_gmail(service=gmail_service)
                         else:
                             logging.warning('Profile and Connection not added')
     else:
