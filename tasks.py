c4d5c89e56435af6513f383d21c4dd3781f8bc6f
AlexanderPease <alexander@usv.com>
2014-10-09 17:53:41 -0400

    WIP test_worker is now entire function of onboard_user

--- tasks.py
+++ tasks.py
@@ -72,6 +72,59 @@ def test_worker(u):
                             # Search Connection database to see if this is a new contact
                             c, created_flag = Connection.objects.get_or_create(user=u,
                                                                             profile=p)
+                            # If newly created Connection, fill it in by
+                            # searching Gmail API
+                            if not created_flag:
+                                logging.info('%s already exists, not updating' % c)
+                            else:
+                                logging.info('Created %s' % c)
+                                # See if any messages match query
+                                emails_in = gmail.ListMessagesMatchingQuery(service=gmail_service, 
+                                                                            user_id='me', 
+                                                                            query= "from:" + p.email)
+                                emails_out = gmail.ListMessagesMatchingQuery(service=gmail_service, 
+                                                                            user_id='me', 
+                                                                            query= "to:" + p.email)
+
+                                # Emails in fields
+                                if emails_in and len(emails_in) > 0:
+                                    c.total_emails_in = len(emails_in)
+
+                                    # Get dates of latest emails in and out
+                                    try:
+                                        latest_email_in = gmail.GetMessage(service=gmail_service, 
+                                                            user_id='me', 
+                                                            msg_id=emails_in[0]['id'])
+                                        latest_email_in_header = gmail.GetMessageHeader(latest_email_in)
+                                        c.latest_email_in_date = gmail.ParseDate(latest_email_in_header['Date'])
+                                    except:
+                                        logging.warning('latest_email_in not added')
+                                else:
+                                    c.total_emails_in = 0
+
+                                # Emails out fields
+                                if emails_out and len(emails_out) > 0:
+                                    c.total_emails_out = len(emails_out)
+                                    try:
+                                        latest_email_out = gmail.GetMessage(service=gmail_service, 
+                                                            user_id='me', 
+                                                            msg_id=emails_out[0]['id'])
+                                        latest_email_out_header = gmail.GetMessageHeader(latest_email_out)
+                                        c.latest_email_out_date = gmail.ParseDate(latest_email_out_header['Date'])
+                                    except:
+                                        logging.warning('latest_email_out not added')
+                                else:
+                                    c.total_emails_out = 0
+
+                                # Finished with all Connection fields
+                                logging.info(c.latest_email_out_date)
+                                logging.info(c.latest_email_in_date)
+                                c.last_updated = datetime.datetime.now()
+                                c.save()
+                        else:
+                            logging.warning('Profile and Connection not added')
+    else:
+        logging.warning('User %s could not log in to Gmail or Contacts API', user)
 
 
 @app.task
