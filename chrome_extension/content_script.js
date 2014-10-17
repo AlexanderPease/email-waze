e1684e264e82c1040f6739f6c125bb763b0f6e44
AlexanderPease <alexander@usv.com>
2014-10-17 15:28:03 -0400

    Show Connection when searched by email in chrome extension

--- chrome_extension/content_script.js
+++ chrome_extension/content_script.js
@@ -31,48 +31,6 @@ function email_from_string(str) {
 }
 
 
-function render_connection(contact) {
-    var $panel = $('td.Bu.y3 div.nH.adC');
-    if ($panel) {
-        $.ajax({
-            type: 'GET',
-            url: 'chrome-extension://'+encodeURIComponent(ID)+'/templates/profile.html',
-            cache: true,
-            dataType: 'text',
-            success: function(html) {
-                var email = contact.email;
-                var email_parts = contact.email.split('@');
-                if (email_parts.length == 2) {
-                    contact.email_name = email_parts[0];
-                    contact.email_domain = email_parts[1];
-                }
-                var $div = $panel.find('div#rapporto');
-                console.log($div)
-                if (!$div.length)
-                    $div = $('<div id="rapporto" style="position:relative;" />');
-                $div.html(Mustache.render(html, contact));
-                $panel.prepend($div);
-                /*
-                var src = lscache.get('p:'+ email);
-                if (src) {
-                    replace_image($div, src);
-                } else {
-                    first_valid_image(contact.images.sort(compare_images), function(src) {
-                        if (!src)
-                            src = 'chrome-extension://'+encodeURIComponent(ID)+'/img/profile.png';
-                        lscache.set('p:'+contact.email, src);
-                        replace_image($div, src);
-                    });
-                }
-                */
-            },
-        });
-    } else {
-        console.log($panel);
-    }
-}
-
-
 function render_profile(contact) {
     var $panel = $('td.Bu.y3 div.nH.adC');
     if ($panel) {
@@ -114,6 +72,49 @@ function render_profile(contact) {
     }
 }
 
+
+function render_connection(contact) {
+    console.log('render_connection()');
+    var $panel = $('td.Bu.y3 div.nH.adC');
+    if ($panel) {
+        $.ajax({
+            type: 'GET',
+            url: 'chrome-extension://'+encodeURIComponent(ID)+'/templates/connection.html',
+            cache: true,
+            dataType: 'text',
+            success: function(html) {
+                var email = contact.email;
+                var email_parts = contact.email.split('@');
+                if (email_parts.length == 2) {
+                    contact.email_name = email_parts[0];
+                    contact.email_domain = email_parts[1];
+                }
+                var $div = $panel.find('div#rapporto');
+                if (!$div.length)
+                    $div = $('<div id="rapporto" style="position:relative;" />');
+                $div.html(Mustache.render(html, contact));
+                $panel.prepend($div);
+                /*
+                var src = lscache.get('p:'+ email);
+                if (src) {
+                    replace_image($div, src);
+                } else {
+                    first_valid_image(contact.images.sort(compare_images), function(src) {
+                        if (!src)
+                            src = 'chrome-extension://'+encodeURIComponent(ID)+'/img/profile.png';
+                        lscache.set('p:'+contact.email, src);
+                        replace_image($div, src);
+                    });
+                }
+                */
+            },
+        });
+    } else {
+        console.log($panel);
+    }
+}
+
+
 var PRIORITY = {
     'Gravatar': 1,
     'LinkedIn': 2,
@@ -123,6 +124,7 @@ var PRIORITY = {
     'Twitter': 6,
     'AngelList': 7,
 };
+
 
 function compare_images(a, b) {
     var aa = PRIORITY[a['service']] || a;
@@ -175,10 +177,9 @@ function start() {
         if (email) {
             console.log('Mouse over ' + email);
             chrome.extension.sendMessage({name:'get_connection_by_email', email:email}, function(data) {
-                if (data['data']) {
-                    var connections = data.data;
-                    console.log(connections)
-                    render_connection(JSON.parse(connections));
+                if (data.data) {
+                    var connection = data.data;
+                    render_connection(connection);
                 }
             });
             /*
