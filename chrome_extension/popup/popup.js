e98b0c1915e1ad06f9009c79a3e441ee3a48fea8
AlexanderPease <alexander@usv.com>
2014-11-29 19:05:26 -0500

    Popup displaying URL from current tab

--- chrome_extension/popup/popup.js
+++ chrome_extension/popup/popup.js
@@ -1,3 +1,39 @@
+var ROOT_URL = 'http://ansatz.me/'
+
 $(document).ready(function(){
-  console.log('ready');
+  chrome.tabs.getSelected(null, function(tab) {
+    var domain = get_domain(tab.url);
+    $('#title').text('Searching for...' + domain);
+    var options = {
+      type: 'GET',
+      url: ROOT_URL + 'api/test',
+      dataType: 'json',
+      success: function(response) {
+        console.log(response);
+      },
+      error: function(response) {
+        console.warn(response);
+      }
+    };
+    console.log('Requesting: ' + options.url)
+    $.ajax(options);
+  });
 });
+
+
+// Just splits out base url from href
+function get_domain(url) {
+  if (url.indexOf("http://") != -1) {
+    url = url.split("http://")[1];
+  }
+  else if (url.indexOf("https://") != -1) {
+    url = url.split("https://")[1];
+  }
+  if (url.indexOf("www.") != -1) {
+    url = url.split("www.")[1];
+  }
+  if (url.indexOf("/") != -1) {
+    url = url.split("/")[0];
+  }
+  return url;
+}
