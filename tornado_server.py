358a58d1434551bb91f26889500134256a0a1f5a
AlexanderPease <alexander@usv.com>
2014-12-17 18:33:18 -0500

    Change 1.0 to 1 in versioning API

--- tornado_server.py
+++ tornado_server.py
@@ -44,10 +44,10 @@ class Application(tornado.web.Application):
       (r"/api/test", app.api.Test),
 
       # Public API v1
-      (r"/api/1.0/profilesearch", app.api.ProfileSearch),
-      (r"/api/1.0/profilebyemail", app.api.ProfileByEmail),
-      (r"/api/1.0/connectionsearch", app.api.ConnectionSearch),
-      (r"/api/1.0/connectionbyemail", app.api.ConnectionByEmail),
+      (r"/api/1/profilesearch", app.api.ProfileSearch),
+      (r"/api/1/profilebyemail", app.api.ProfileByEmail),
+      (r"/api/1/connectionsearch", app.api.ConnectionSearch),
+      (r"/api/1/connectionbyemail", app.api.ConnectionByEmail),
 
       (r"/api/group/(?P<group_id>[A-z-+0-9]+)/acceptinvite", app.group_api.AcceptInvite),
       (r"/api/group/create", app.group_api.CreateGroup),
