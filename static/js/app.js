f074e12fd5e855501d7831030a20050ff53bb47b
AlexanderPease <alexander@usv.com>
2014-12-22 22:22:39 -0500

    WIP tutorial using $ calls for posts, not static data

--- static/js/app.js
+++ static/js/app.js
@@ -28,9 +28,20 @@ App.Router.reopen({
   location: 'hash'
 });
 
+App.SearchRoute = Ember.Route.extend({
+  model: function() {
+    return 'foo';
+  }
+});
+
 App.PostsRoute = Ember.Route.extend({
   model: function() {
-    return posts;
+    return $.getJSON('http://tomdale.net/api/get_recent_posts/?callback=?').then(function(data){
+      return data.posts.map(function(post){
+        post.body = post.content;
+        return post;
+      });
+    });
   }
 });
 
