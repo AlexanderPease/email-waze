9429a76493f52a52e33a55ccff9fcc3eccb6b13f
AlexanderPease <alexander@usv.com>
2015-01-14 14:37:27 -0500

    WIP whitespace delimited simple search is not possible via q parameter in public_api.py call

--- app/public_api.py
+++ app/public_api.py
@@ -1,5 +1,6 @@
 import app.basic, settings, ui_methods, tornado.web
 import logging
+from mongoengine.queryset import Q
 from db.profiledb import Profile
 from db.userdb import User
 from db.connectiondb import Connection
@@ -24,10 +25,34 @@ class SearchBaseProfileConnection(app.basic.BaseHandler):
         except:
             return self.api_error(500, 'Could not find client user in database')
 
+        # Simple search
+        q = self.get_argument('q', '')
+
+        # Advanced search
         name = self.get_argument('name', '')
         domain = self.get_argument('domain', '')
+        company = self.get_argument('company', '')
 
-        if name or domain:
+        # Check for no parameters
+        if not q and not name and not domain:
+            return self.api_error(400, 'Did not include query parameters')
+
+        # Default to simple search if present
+        if q:
+            q_array = q.split(" ") # whitespace delimited
+            profiles = Profile.objects(__raw__={
+                "$or": [
+                    {
+                        "name": {"$in": q_array}
+                    },
+                    {
+                        "domain": {"$in": q_array}
+                    }
+                ]
+            })
+            logging.info(profiles)
+
+        else:
             # Global profile results
             profiles = Profile.objects(name__icontains=name, email__icontains=domain).order_by('name') # case-insensitive contains
 
@@ -35,22 +60,19 @@ class SearchBaseProfileConnection(app.basic.BaseHandler):
             if len(profiles) == 0:
                 return self.api_response(data={})
 
-            # Connections
-            group_users = current_user.all_group_users()
-            connections = Connection.objects(profile__in=profiles, user__in=group_users).order_by('-latest_email_out_date')
+        # Connections
+        group_users = current_user.all_group_users()
+        connections = Connection.objects(profile__in=profiles, user__in=group_users).order_by('-latest_email_out_date')
 
-            # BaseProfileConnections
-            ps = []
-            for p in profiles:
-                bp = BaseProfileConnection(p)
-                cs = Connection.objects(profile=p, user__in=group_users).order_by('-latest_email_out_date')
-                if len(cs) > 0:
-                    bp.connections = cs
-                    bp.latest_email_out_date = cs[0]
-                    ps.append(bp)
+        # BaseProfileConnections
+        ps = []
+        for p in profiles:
+            bp = BaseProfileConnection(p)
+            cs = Connection.objects(profile=p, user__in=group_users).order_by('-latest_email_out_date')
+            if len(cs) > 0:
+                bp.connections = cs
+                bp.latest_email_out_date = cs[0]
+                ps.append(bp)
 
-            return self.api_response(data={"profiles": list_to_json_list(ps)})
-
-        else:
-            return self.api_error(400, 'Did not include query parameters')
+        return self.api_response(data={"profiles": list_to_json_list(ps)})
 
