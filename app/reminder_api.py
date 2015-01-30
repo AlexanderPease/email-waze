a9900bd980baab426a413dfe30cec23fb3c4c0af
AlexanderPease <alexander@usv.com>
2015-01-30 18:37:15 -0500

    WIP /api/reminder/create. Need to test'

---
+++ app/reminder_api.py
@@ -0,0 +1,47 @@
+import app.basic, settings, ui_methods, tornado.web
+import logging
+from db.reminderdb import Reminder
+from db.userdb import User
+
+########################
+### User deletes his/her account
+### /api/reminder/create
+########################
+class CreateReminder(app.basic.BaseHandler):
+    @tornado.web.authenticated
+    def post(self, user_id):
+        if not self.current_user:
+            return self.api_error(401, 'User is not logged in')
+        try:
+            u = User.objects.get(email=self.current_user)
+        except:
+            return self.api_error(500, 'Could not find client user in database')
+        profile_id = self.get_argument('profile_id', '')
+        company_id = self.get_argument('company_id', '')
+        days = self.get_argument('days', '')
+        recurring = self.get_argument('recurring', '')
+
+        # Require necessary fields
+        if profile_id and company_id:
+            return self.api_error(400, 'Can not set both company and profile')
+        elif not profile_ud and not company_id: 
+            return self.api_error(400, 'Must have either company or profile')
+        if not days:
+            return self.api_error(400, 'Must set a reminder time period')
+
+        # Save reminder
+        if profile_id:
+            try:
+                r = Reminder(profile=p, user=u, days=days, reucrring=recurring)
+            except: 
+                return self.api_error(501, 'Error saving profile reminder')
+        elif company_id:
+            try:
+                r = Reminder(company=c, user=u, days=days, recurring=recurring)
+            except: 
+                return self.api_error(501, 'Error saving profile reminder')
+        return self.api_response(data={})
+
+
+
+
