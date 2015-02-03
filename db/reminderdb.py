a7051f4a12bfc8f16dc276d338a635f3c6231a3a
AlexanderPease <alexander@usv.com>
2015-02-02 22:52:03 -0500

    WIP try breaking reminders out into two different classes

--- db/reminderdb.py
+++ db/reminderdb.py
@@ -5,17 +5,8 @@ from profiledb import Profile
 from companydb import Company
 import logging, datetime
 
-mongo_database = settings.get('mongo_database')
-connect('reminder', host=mongo_database['host'])
-
 class Reminder(Document):
-    # The User who set the reminder
     user = ReferenceField(User, required=True)
-
-    # Reminder is either to contact a Profile or Company
-    # One of these is required
-    profile = ReferenceField(Profile)
-    company = ReferenceField(Company)
 
     # Recurring reminder or not
     recurring = BooleanField(required=True, default=False)
@@ -26,43 +17,33 @@ class Reminder(Document):
     # Number of days until reminder is due, or number of days for recurring time period
     days = IntField(required=True)
 
-    def __str__(self):
-        if self.profile:
-            return 'Reminder: User %s <-> Profile %s' % (self.user, self.profile)
-        elif self.company:
-            return 'Reminder: User %s <-> Company %s' % (self.user, self.company)
-        else:
-            raise Exception
-
-    def save(self, *args, **kwargs):
-        # Ensure either company or profile is set
-        if not self.profile and not self.company:
-            msg = 'Reminder requires either profile or company to be set'
-            return
-        elif self.profile and self.company:
-            msg = 'Reminder cannot have both company and profile set'
-            return
-        # Ensure sparse uniqueness. Couldn't get index to work
-        if self.profile:
-            rs = Reminder.objects(user=self.user, profile=self.profile)
-        else:
-            rs = Reminder.objects(user=self.user, company=self.company)
-        if rs:
-            if len(rs) > 1:
-                raise Exception
-            r = rs[0]
-            if r.id != self.id:
-                msg = 'Reminder for %s & %s/%s already exists' % (self.user, self.profile, self.company)
-                return 
-
-        super(Reminder, self).save(*args, **kwargs)
-
-
-
     def to_json(self):
         json = {}
         for k,v in Reminder._fields.iteritems():
             logging.info(self.k)
             logging.info(self.v) # ??
         return json
+
+class CompanyReminder(Reminder):
+    company = ReferenceField(Company, required=True)
+    meta = {
+        'indexes': [
+            {'fields': ['user', 'company'], 'unique': True},
+        ]
+    }
+
+    def __str__(self):
+        return 'Reminder: User %s <-> Company %s' % (self.user, self.company)
+
+class ProfileReminder(Reminder):
+    profile = ReferenceField(Profile, required=True)
+    meta = {
+        'indexes': [
+            {'fields': ['user', 'profile'], 'unique': True},
+        ]
+    }
+
+    def __str__(self):
+        return 'Reminder: User %s <-> Profile %s' % (self.user, self.profile)
+
 
