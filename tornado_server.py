ce88570574208aa961e810be47d439418db07470
AlexanderPease <alexander@usv.com>
2015-03-04 18:51:31 -0500

    Remove app/group.py dependcy from tornado_settings.py

--- tornado_server.py
+++ tornado_server.py
@@ -11,7 +11,7 @@ import settings
 import templates
 
 import app.basic, app.public, app.admin, app.email_handler
-import app.googleauth, app.user, app.group, app.stripe_handler
+import app.googleauth, app.user, app.stripe_handler
 import app.api, app.group_api, app.public_api, app.user_api
 import app.reminder_api
 
